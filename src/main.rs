573a507a34a59e2ade12f1216620565c46174c1d
hpr <hpr.caeco@gmail.com>
2025-02-24 22:46:35 -0300

    Aula atualizada

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+//cargo new, cria novo projeto
+//cargo init, cria novo projeto em uma pasta existente
+//cargo help, help do cargo
+
+use core::time;
+use std::thread::sleep;
+
 // declaração de constante não é variavel
 const PI:f32 = 3.1415;
 // declaração de uma variavel global imutavel
@@ -29,6 +36,66 @@ fn var() {
     
 }
 
+fn idade (idade:i8, responsavel:bool) { //condicionais
+    
+    let check = idade > 18;
+    println!("Maior de 18: {}", check);
+    
+    if idade > 18 || idade>= 16 && responsavel{
+        println!("Pode entrar")
+    }else {
+        println!("Não pode")
+    }
+    
+    let result  = if idade > 18 || idade>= 16 && responsavel { println!("Pode entrar"); } else { println!("Não pode"); };
+
+}
+
+fn counter(aux:i8) { // repetições
+
+    let mut rep:i8 = 0;
+
+    while rep < aux {
+        println!("Contando: {}", rep);
+        sleep(time::Duration::from_millis(10));
+        rep += 1;
+    }
+
+    rep = 0;
+
+    loop { // = while true
+        
+        sleep(time::Duration::from_millis(10));
+        
+        if  rep == 10 {
+            break;
+        }
+        println!("Contando2:    {}", rep);
+        rep += 1;
+    }
+
+    
+}
+
+fn interval(aux:i8) {
+
+    for a in 1..aux  { // mesma notação for a in 1..=aux
+        println!("Contando: {}", a);
+        sleep(time::Duration::from_millis(10));
+    }
+
+}
+
+fn check_statment(aux: &str) { 
+
+    match aux {
+        "PHP" => println!("Linguagem WEB."), 
+        "Kotlin" => println!("Android"),
+        _ => println!("Other case"),
+    }
+}
+
+
 
 fn main() {
 println!("Working!");
@@ -44,6 +111,11 @@ println!("Variavel global: {}", GLOBAL_VAR);
 //    println!("Valor CTE mutavel: {}", GLOBAL_VAR_MUT);  não existe mais
 //}
 
+//idade(16, true);
 
+//counter(15);
+
+interval(15);
+check_statment("PHP");
 
 }
